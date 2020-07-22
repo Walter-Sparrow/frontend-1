1b716c7f5294d93d41e8ef8c966f47e640bef26a
Walter-Sparrow <53474572+Walter-Sparrow@users.noreply.github.com>
2020-07-22 16:15:26 +0300

    init project

---
+++ assets/js/scripts.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function () {
+    /* Player */
+    String.prototype.toMMSS = function () {
+        var sec_num = parseInt(this, 10);
+        var minutes = Math.floor(sec_num / 60);
+        var seconds = sec_num - (minutes * 60);
+
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        return minutes + ':' + seconds;
+    }
+
+    let video = document.getElementById("presentation-video"),
+        btn = document.getElementById("btn_icon"),
+        video_len = document.getElementById("video-len"),
+        player = document.getElementById("player"),
+        video_info = document.getElementById("video-info");
+
+    video.addEventListener("loadedmetadata", function () {
+        video_len.innerText = video.duration.toString().toMMSS();
+    })
+
+    video.addEventListener("ended", function () {
+        video_info.style.display = 'initial';
+        initialPlayer();
+    })
+
+    btn.parentElement.addEventListener("click", function () {
+        if (video.paused) {
+            video.play();
+            video_info.style.opacity = '0';
+            btn.src = "assets/images/presentation/button-stop.png";
+            video.classList.remove("paused");
+            player.classList.remove("paused");
+        } else {
+            video.pause();
+            initialPlayer();
+        }
+    })
+
+    function initialPlayer() {
+        video.classList.add("paused");
+        player.classList.add("paused");
+        video_info.style.opacity = '1';
+        btn.src = "assets/images/presentation/button-play.png";
+        video_len.innerText = video.currentTime.toString().toMMSS();
+    }
+});
